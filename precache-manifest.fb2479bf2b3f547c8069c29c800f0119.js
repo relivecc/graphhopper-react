1df8c44b252af0d283328eb623a19d942da28fee
Yousef El-Dardiry <yousefdardiry@gmail.com>
2020-06-09 00:08:52 +0200

    Updates

---
+++ precache-manifest.fb2479bf2b3f547c8069c29c800f0119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5971556be6331c96c76565d8676d45",
+    "url": "/graphhopper-react/index.html"
+  },
+  {
+    "revision": "fe7052b17f8dc2ba9273",
+    "url": "/graphhopper-react/static/css/2.2bd37e2f.chunk.css"
+  },
+  {
+    "revision": "5b3fec2494b439b5f5d4",
+    "url": "/graphhopper-react/static/css/main.2305ceae.chunk.css"
+  },
+  {
+    "revision": "fe7052b17f8dc2ba9273",
+    "url": "/graphhopper-react/static/js/2.b957c6a2.chunk.js"
+  },
+  {
+    "revision": "3c0753b3e940f20f87b54f724c06ade9",
+    "url": "/graphhopper-react/static/js/2.b957c6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3fec2494b439b5f5d4",
+    "url": "/graphhopper-react/static/js/main.4b6f7447.chunk.js"
+  },
+  {
+    "revision": "84ed48936a13ba2d5389",
+    "url": "/graphhopper-react/static/js/runtime-main.395663ff.js"
+  }
+]);
